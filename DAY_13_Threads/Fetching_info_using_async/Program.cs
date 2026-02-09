228086486c1a42481f423f9d80b58b5a170cec6f
Manohar9392 <vmanureddy428@gmail.com>
2026-02-09 22:41:30 +0530

    Solved Async await problems on feb_9

---
+++ DAY_13_Threads/Fetching_info_using_async/Program.cs
@@ -0,0 +1,84 @@
+﻿using System.Net.NetworkInformation;
+using static System.Net.WebRequestMethods;
+
+
+    //using System;
+    //using System.Threading.Tasks;
+
+    //class Program
+    //{
+    //    static async Task Main()
+    //    {
+    //        await SaveAsync();                // Task (no return)
+    //        int total = await GetTotalAsync(); // Task<int> (returns value)
+    //        Console.WriteLine(total);
+    //    }
+
+    //    static async Task SaveAsync()
+    //    {
+    //        await Task.Delay(3000); // pretend we saved to DB
+    //        Console.WriteLine("Saved!");
+    //    }
+
+    //    static async Task<int> GetTotalAsync()
+    //    {
+    //        await Task.Delay(300); // pretend we calculated
+    //        return 42;
+    //    }
+    //}
+
+
+
+namespace AsyncAwaitExample2
+{
+    using System.Net.Http;
+
+    public class User
+    {
+        private async Task FetchJsonAsync()
+        {
+            try
+            {
+                string url = "https://jsonplaceholder.typicode.com/todos/1";
+                string json = await GetAsync(url);
+                Console.WriteLine(json);
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine(ex.Message);
+            }
+        }
+
+        private async Task FetchGoogleAsync()
+        {
+            try
+            {
+                string url = "https://google.com/";
+                string json = await GetAsync(url);
+                Console.WriteLine(json);
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine(ex.Message);
+            }
+        }
+
+        private static async Task<string> GetAsync(string url)
+        {
+            using (HttpClient client = new HttpClient())
+            {
+                HttpResponseMessage response = await client.GetAsync(url);
+                response.EnsureSuccessStatusCode();
+
+                return await response.Content.ReadAsStringAsync();
+            }
+        }
+
+        public static async Task Main()
+        {
+            User u = new User();
+            await u.FetchJsonAsync();
+            await u.FetchGoogleAsync();
+        }
+    }
+}
